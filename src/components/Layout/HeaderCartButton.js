345b1752dc31ff412b9e202626e2e9919946aa72
Barış Sokat <barissokat@outlook.com>
2022-01-10 11:08:17 +0300

    Add Card and Update Meals components

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,7 @@
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 
-const HeaderCartButton = (props) => {
+const HeaderCartButton = props => {
   return (
     <button className={classes.button}>
       <span className={classes.icon}>
